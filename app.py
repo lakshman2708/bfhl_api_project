e71004869330326d9d9db5f3cc66855c0ee16695
Lakshman Shrinath S <lakshmanshrinath.s2022@vitstudent.ac.in>
2025-08-29 12:26:51 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -102,5 +102,4 @@ def bfhl():
         return jsonify({"is_success": False, "error": "Server error", "details": str(e)}), 500
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=False)
+    app.run()
